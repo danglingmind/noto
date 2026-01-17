5d9f65142c885aee9f813022724aaefa0b32add7
Prateek Reddy <prateekreddy274@gmail.com>
2026-01-17 19:57:36 +0530

    added microsoft clarity

--- src/components/google-analytics.tsx
+++ src/components/google-analytics.tsx
@@ -4,31 +4,55 @@ import Script from 'next/script'
 
 export function GoogleAnalytics() {
 	const gaId = process.env.NEXT_PUBLIC_GA_ID
+	const clarityId = process.env.NEXT_PUBLIC_CLARITY_ID
 
-	if (!gaId) {
+	// Return null only if neither analytics tool is configured
+	if (!gaId && !clarityId) {
 		return null
 	}
 
 	return (
 		<>
-			<Script
-				strategy="afterInteractive"
-				src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
-			/>
-			<Script
-				id="google-analytics"
-				strategy="afterInteractive"
-				dangerouslySetInnerHTML={{
-					__html: `
-						window.dataLayer = window.dataLayer || [];
-						function gtag(){dataLayer.push(arguments);}
-						gtag('js', new Date());
-						gtag('config', '${gaId}', {
-							page_path: window.location.pathname,
-						});
-					`,
-				}}
-			/>
+			{/* Google Analytics */}
+			{gaId && (
+				<>
+					<Script
+						strategy="afterInteractive"
+						src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
+					/>
+					<Script
+						id="google-analytics"
+						strategy="afterInteractive"
+						dangerouslySetInnerHTML={{
+							__html: `
+								window.dataLayer = window.dataLayer || [];
+								function gtag(){dataLayer.push(arguments);}
+								gtag('js', new Date());
+								gtag('config', '${gaId}', {
+									page_path: window.location.pathname,
+								});
+							`,
+						}}
+					/>
+				</>
+			)}
+
+			{/* Microsoft Clarity */}
+			{clarityId && (
+				<Script
+					id="microsoft-clarity"
+					strategy="afterInteractive"
+					dangerouslySetInnerHTML={{
+						__html: `
+							(function(c,l,a,r,i,t,y){
+								c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+								t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+								y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+							})(window, document, "clarity", "script", "${clarityId}");
+						`,
+					}}
+				/>
+			)}
 		</>
 	)
 }
