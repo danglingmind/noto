096b1b34f4f25d09cf8a9355821888fc6b30055f
Prateek Reddy <prateekreddy274@gmail.com>
2026-03-22 12:13:16 +0530

    prisma generated client fix

--- generated/prisma/internal/class.ts
+++ generated/prisma/internal/class.ts
@@ -46,6 +46,8 @@ const config: runtime.GetPrismaClientConfig = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
+  "ciName": "Vercel",
   "inlineDatasources": {
     "db": {
       "url": {
